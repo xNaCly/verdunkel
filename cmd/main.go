9188c6676232dac6ef062fd377799cde0a43b03d
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-06-02 13:43:48 +0200

    cmd: configuration via cli flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"regexp"
 
 	"github.com/xnacly/verdunkel"
+	"github.com/xnacly/verdunkel/log"
 )
 
 func main() {
 	c := &verdunkel.Config{}
 
-	// var Default = &Config{
-	// 	Function:   true,
-	// 	Variables:  true,
-	// 	Structs:    true,
-	// 	Interfaces: true,
-	// 	OutputDir:  "./out",
-	// }
+	slog.SetDefault(slog.New(log.New(os.Stdout, slog.LevelDebug)))
+	slog.Debug("startup...")
 
-	// TODO: add all flags
 	flag.BoolVar(&c.Function, "funcs", true, "function name obfuscation")
 	flag.BoolVar(&c.Variables, "vars", true, "variable name obfuscation")
 	flag.BoolVar(&c.Structs, "structs", true, "struct name and field obfuscation")
+	flag.BoolVar(&c.Interfaces, "interfaces", true, "interface name and method name obfuscation")
+	flag.BoolVar(&c.ConstsValue, "consts", false, "const value obfuscation")
+	flag.BoolVar(&c.Logs, "logs", false, "stripping of all fmt.Print, fmt.Println, print, println, slog and log calls")
+	flag.BoolVar(&c.Packages, "packages", false, "package name and path obfuscation")
+	flag.BoolVar(&c.FileNames, "files", false, "file name obfuscation")
+	flag.StringVar(&c.OutputDir, "out", "./out", "file name obfuscation")
+	exclude := flag.String("exclude", "", "matched agains file path, if matched, skips the file")
 	flag.Parse()
+	if *exclude != "" {
+		e, err := regexp.Compile(*exclude)
+		if err != nil {
+			slog.Error("failed to compile exclude regexp", "exp", *exclude, "err", err)
+			return
+		}
+		c.Exclude = e
+	}
+
+	slog.Info("configuration parsing done")
+	fmt.Println(c.String())
 }
